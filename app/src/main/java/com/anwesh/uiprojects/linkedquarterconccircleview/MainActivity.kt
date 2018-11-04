804ea317285a1ee39522f118d83a805fd409899d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 10:43:37 +0530

    adding a app module

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterconccircleview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anwesh.uiprojects.linkedquarterconccircleview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
