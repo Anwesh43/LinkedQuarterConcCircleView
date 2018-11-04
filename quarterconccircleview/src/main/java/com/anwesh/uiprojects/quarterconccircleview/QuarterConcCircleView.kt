b88a1f8c24ea3de2266eb450f34ea8ebda71f48d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 11:19:34 +0530

    adding QCCNode class

--- quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
+++ quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
@@ -124,4 +124,49 @@ class QuarterConcCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QCCNode(var i : Int, val state : State = State()) {
+
+        private var next : QCCNode? = null
+
+        private var prev : QCCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = QCCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQCCNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QCCNode {
+            var curr : QCCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
