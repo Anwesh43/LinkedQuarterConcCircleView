b64ac5919a4f849faa06a7f80608ea6f357a4dc0
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 10:49:29 +0530

    adding QuarterConcCircleView class

--- quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
+++ quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
@@ -14,3 +14,21 @@ import android.graphics.RectF
 import android.app.Activity
 
 val nodes : Int = 5
+
+class QuarterConcCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
