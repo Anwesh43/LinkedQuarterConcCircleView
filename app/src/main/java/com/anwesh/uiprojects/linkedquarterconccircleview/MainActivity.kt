e74731aeb9744877af71df3a118e48aa2c35c1b1
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 11:59:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquarterconccircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterconccircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedquarterconccircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.quarterconccircleview.QuarterConcCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        QuarterConcCircleView.create(this)
     }
 }
