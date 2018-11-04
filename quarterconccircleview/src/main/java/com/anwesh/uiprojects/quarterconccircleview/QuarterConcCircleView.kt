209b5f5e163abdd50a17cbb111a9c089829d3741
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 10:47:24 +0530

    importing libraries and defining nodes

--- quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
+++ quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.quarterconccircleview
 /**
  * Created by anweshmishra on 04/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+
+val nodes : Int = 5
