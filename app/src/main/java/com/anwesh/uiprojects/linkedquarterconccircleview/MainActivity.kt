6e526a0ddf56248356f9839a30fe3d0dd0fd60ef
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 12:26:58 +0530

    using onAnimationCompletedListener in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquarterconccircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterconccircleview/MainActivity.kt
@@ -3,14 +3,25 @@ package com.anwesh.uiprojects.linkedquarterconccircleview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.quarterconccircleview.QuarterConcCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        QuarterConcCircleView.create(this)
+        val view : QuarterConcCircleView = QuarterConcCircleView.create(this)
         fullScreen()
+        view.addOnAnimationCompleteListener({
+            createToast("Animation number ${it} completed")
+        }) {
+            createToast("Animation number $it is reset")
+        }
+
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
