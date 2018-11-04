a705ed85f7ffa0dce8cb0169b6914e2ee51ace31
Anwesh43 <anweshthecool0@gmail.com>
2018-11-04 10:58:01 +0530

    adding State class

--- quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
+++ quarterconccircleview/src/main/java/com/anwesh/uiprojects/quarterconccircleview/QuarterConcCircleView.kt
@@ -12,8 +12,16 @@ import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.RectF
 import android.app.Activity
+import android.util.Log
 
 val nodes : Int = 5
+
+val lines : Int = 4
+
+val SCALE_GAP : Float = 0.05f
+fun Float.scaleFactor() : Float = Math.floor(this/0.5).toFloat()
+
+fun Float.scaleMultiplier(dir : Float) : Float = (scaleFactor()+ (1 - scaleFactor()) / lines) * dir * SCALE_GAP
 
 class QuarterConcCircleView(ctx : Context) : View(ctx) {
 
@@ -22,7 +30,7 @@ class QuarterConcCircleView(ctx : Context) : View(ctx) {
     override fun onDraw(canvas : Canvas) {
 
     }
-    
+
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -30,5 +38,27 @@ class QuarterConcCircleView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            val k : Float = scale.scaleMultiplier(dir)
+            scale += k
+            Log.d("scale multiplier", "${k}")
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
